b1e0dd5d26b947da3bad5ce02def2feec40e3e59
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-01-12 19:10:04 +0000

    photo button

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150111220008) do
+ActiveRecord::Schema.define(version: 20150112165852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,14 +29,11 @@ ActiveRecord::Schema.define(version: 20150111220008) do
   add_index "comments", ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id", using: :btree
 
   create_table "photos", force: true do |t|
-    t.text     "caption"
+    t.string   "caption"
     t.integer  "place_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "picture"
   end
-
-  add_index "photos", ["place_id"], name: "index_photos_on_place_id", using: :btree
 
   create_table "places", force: true do |t|
     t.string   "name"
