ddb941723489371fad534665da2667e6ee4c871a
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-01-14 17:55:48 +0000

    production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,5 @@
 Nomster::Application.configure do
 
-  config.action_mailer.default_url_options = { :host => 'nomster-hilary.herokuapp.com'}
-
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
@@ -12,7 +10,7 @@ Nomster::Application.configure do
     :enable_starttls_auto => true
   }
 
- 
+ config.action_mailer.default_url_options = { :host => 'nomster-hilary.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
